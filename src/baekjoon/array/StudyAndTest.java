10666d7bff4a4382ba014ad402e9377e4b707928
sem_in_github <semin6634@naver.com>
2025-01-21 14:03:08 +0900

    array: 자바의 정석 익명클래스, 람다식, 함수형인터페이스 공부

--- src/baekjoon/array/StudyAndTest.java
+++ src/baekjoon/array/StudyAndTest.java
@@ -15,6 +15,54 @@ public class StudyAndTest {
 		
 		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);//대소문자 구분을 없애줌
 		System.out.println(Arrays.toString(strArr));
+		
+		
+		/*<자바의 정석> 익명클래스, 람다식, 함수형인터페이스
+		 * 람다식은 익명함수가 아니라 익명객체이다!->2회성
+		 * */
+		
+		/*
+		//1. 익명 클래스
+		Object obj = new Object() {
+			int max(int a, int b) {
+				return a>b ? a : b;
+			}
+		};
+		
+		int value = obj.max(3,5); //에러 
+		*/
+		
+		//2. 함수형 인터페이스 + 익명 객체 생성
+		MyFunction f1 = new MyFunction() {
+			//int max(int a, int b) {//오버라이딩 규칙: 접근지정자를 좁게 바꿀 수 없음
+			public int max(int a, int b) {
+				return a>b ? a : b;
+			} 
+		};
+		
+		//3. 함수형 인터페이스 + 람다식(익명 객체 생성): 훨씬 더 간결해짐
+		// 람다식(익명 객체)을 다루기 위한
+		// 참조변수의 타입은 함수형 인터페이스를 기반으로 한다.
+		int value = f1.max(3, 5);
+		System.out.println(value);
+		
+		MyFunction f2 = (a, b) -> a > b ? a : b;
+		System.out.println(f2.max(3, 5));
+		
+		//4. 활용 예시
+		//람다 사용X
+		List<String> list1 = Arrays.asList("abc","aaa","bbb","ddd","aaa");
+		Collections.sort(list1, new Comparator<String>(){
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		System.out.println(list1.toString());
+		
+		//람다 사용O
+		List<String> list2 = Arrays.asList("abc","aaa","bbb","ddd","aaa");
+		Collections.sort(list2, (o1, o2)->o1.compareTo(o2));
+		System.out.println(list2.toString());
 	}
 
 }
@@ -29,4 +77,9 @@ class Descending implements Comparator{
 		//return -1
         throw new IllegalArgumentException("Arguments must implement Comparable");
 	}
+}
+
+@FunctionalInterface//함수형 인터페이스라는 뜻이면서 아래의 인터페이스가 단 하나의 추상메서드만 갖는지 체크해줌
+interface MyFunction{
+	public abstract int max(int a, int b);
 }
