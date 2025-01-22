bdb35b4d798e8af36c3530d4080d3da1faf2c370
sem_in_github <semin6634@naver.com>
2025-01-22 13:22:42 +0900

    bfs: n4179

--- src/baekjoon/bfs/n4179.java
+++ src/baekjoon/bfs/n4179.java
@@ -6,75 +6,79 @@ import java.io.*;
  * */
 
 public class n4179 {
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		char[][] graph = new char[R][C];
-		int[][] jGraph = new int[R][C];
-		int[][] fGraph = new int[R][C];
-		for(int i=0; i<R; i++) {
-			Arrays.fill(jGraph[i], -1);
-			Arrays.fill(fGraph[i], -1);
-		}
-		
-		Queue<int[]> qf = new ArrayDeque<>();
-		Queue<int[]> qj = new ArrayDeque<>();
-		for(int i=0; i<R; i++) {
-			String input = br.readLine();
-			for(int j=0; j<C; j++) {
-				char c = input.charAt(j);
-				graph[i][j] = c;
-				if(c == 'J') {
-					qj.add(new int[]{i, j});
-					jGraph[i][j] = 0;
-				}
-				else if(c == 'F') {
-					qf.add(new int[]{i, j});
-					fGraph[i][j] = 0;
-				}
-			}
-		}
-		
-		while(!qf.isEmpty()) {
-			int[] cur = qf.poll();
-			for(int i=0; i<4; i++) {
-				int nx = cur[0] + dx[i];
-				int ny = cur[1] + dy[i];
-				if(0<=nx && nx<R && 0<=ny && ny<C
-						&& graph[nx][ny] != '#'
-						&& fGraph[nx][ny] == -1) {
-					fGraph[nx][ny] = fGraph[cur[0]][cur[1]] + 1;
-					qf.add(new int[] {nx, ny});
-				}
-			}
-		}
-		
-		while(!qj.isEmpty()) {
-			int[] cur = qj.poll();
-			for(int i=0; i<4; i++) {
-				int nx = cur[0] + dx[i];
-				int ny = cur[1] + dy[i];
-				if(nx<0 || nx>=R || ny<0 || ny>=C) {
-					System.out.println(jGraph[cur[0]][cur[1]] + 1);
-					return;
-				}
-				else if(graph[nx][ny] != '#'&& jGraph[nx][ny] == -1) {
-					if(jGraph[cur[0]][cur[1]] + 1 < fGraph[nx][ny]) {
-						jGraph[nx][ny] = jGraph[cur[0]][cur[1]] + 1;
-						qj.add(new int[] {nx, ny});
-					}
-				}
-			}
-		}
-		
-		System.out.println("IMPOSSIBLE");
-	}
+    static int[][] dist1; // 불의 전파 시간
+    static int[][] dist2; // 지훈이의 이동 시간
+    static String[] board; // 미로
+    static int n, m; // 미로의 크기
+    static int[] dx = {1, 0, -1, 0}; // 이동 방향 (x축)
+    static int[] dy = {0, 1, 0, -1}; // 이동 방향 (y축)
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        dist1 = new int[n][m];
+        dist2 = new int[n][m];
+        board = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist1[i], -1);
+            Arrays.fill(dist2[i], -1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            board[i] = br.readLine();
+        }
+
+        Queue<int[]> Q1 = new LinkedList<>(); // 불의 전파를 위한 BFS 큐
+        Queue<int[]> Q2 = new LinkedList<>(); // 지훈이의 이동을 위한 BFS 큐
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i].charAt(j) == 'F') {
+                    Q1.add(new int[]{i, j});
+                    dist1[i][j] = 0;
+                }
+                if (board[i].charAt(j) == 'J') {
+                    Q2.add(new int[]{i, j});
+                    dist2[i][j] = 0;
+                }
+            }
+        }
+
+        // 불의 전파 BFS
+        while (!Q1.isEmpty()) {
+            int[] cur = Q1.poll();
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cur[0] + dx[dir];
+                int ny = cur[1] + dy[dir];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (dist1[nx][ny] >= 0 || board[nx].charAt(ny) == '#') continue;
+                dist1[nx][ny] = dist1[cur[0]][cur[1]] + 1;
+                Q1.add(new int[]{nx, ny});
+            }
+        }
+
+        // 지훈이의 이동 BFS
+        while (!Q2.isEmpty()) {
+            int[] cur = Q2.poll();
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cur[0] + dx[dir];
+                int ny = cur[1] + dy[dir];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) { // 탈출 성공
+                    System.out.println(dist2[cur[0]][cur[1]] + 1);
+                    return;
+                }
+                if (dist2[nx][ny] >= 0 || board[nx].charAt(ny) == '#') continue;
+                if (dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur[0]][cur[1]] + 1) continue;
+                dist2[nx][ny] = dist2[cur[0]][cur[1]] + 1;
+                Q2.add(new int[]{nx, ny});
+            }
+        }
+
+        // 탈출 실패
+        System.out.println("IMPOSSIBLE");
+    }
 }
