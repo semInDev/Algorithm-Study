fbad0924e4294c3bd981e8400bc369bd95e03837
sem_in_github <semin6634@naver.com>
2025-01-27 15:19:19 +0900

    bst: n7662 정답 풀이

--- src/baekjoon/bst/n7662.java
+++ src/baekjoon/bst/n7662.java
@@ -9,29 +9,42 @@ public class n7662 {
 
 		int T = Integer.parseInt(br.readLine());
 		while(T-->0) {
-			TreeSet<Integer> bst = new TreeSet<>((o1,o2)->o2.compareTo(o1));
-			//같은 수가 들어가는 예외가 발생할 수 있음 -> 같은 수가 들어가면 이를 따로 기록해주는 방법도 있음
+			TreeMap<Integer,Integer> bst = new TreeMap<>((o1,o2)->o2.compareTo(o1));
 			int k = Integer.parseInt(br.readLine());
 			while(k-->0) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				String cmd = st.nextToken();
 				int num = Integer.parseInt(st.nextToken());
 				if(cmd.equals("I")) {
-					bst.add(num);
+					if(!bst.isEmpty() && bst.containsKey(num)) {
+						bst.put(num, bst.get(num)+1);
+					}else {
+						bst.put(num, 1);
+					}
 				}else {//cmd == "D"
 					if(bst.isEmpty())
 						continue;
 					else if(num == 1) {
-						bst.pollFirst();
+						int first_num = bst.firstKey();
+						if(bst.get(first_num)==1) {
+							bst.pollFirstEntry();
+						}else {
+							bst.put(first_num, bst.get(first_num)-1);
+						}
 					}else {//num==-1
-						bst.pollLast();
+						int last_num = bst.lastKey();
+						if(bst.get(last_num)==1) {
+							bst.pollLastEntry();
+						}else {
+							bst.put(last_num, bst.get(last_num)-1);
+						}
 					}
 				}
 			}
 			if(bst.isEmpty())
 				sb.append("EMPTY\n");
 			else {
-				sb.append(bst.first()+" "+bst.last()+"\n");
+				sb.append(bst.firstKey()+" "+bst.lastKey()+"\n");
 			}
 		}
 		System.out.print(sb);
