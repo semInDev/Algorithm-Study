676b6cb527830ee044c88da77851baffd78094dd
sem_in_github <semin6634@naver.com>
2025-03-06 13:11:34 +0900

    icoda q2 (가독성 개선)

--- src/icoda/greedy/q2.java
+++ src/icoda/greedy/q2.java
@@ -13,8 +13,9 @@ public class q2 {
 		int current;
 		for(int i=1; i<str.length(); i++) {
 			current = Integer.parseInt(str.charAt(i)+"");
-			if(result == 0 || result == 1
-					|| current == 0 || current == 1) {
+			/*if(result == 0 || result == 1
+					|| current == 0 || current == 1) {*/
+			if(result <= 1 || current <= 1) {
 				result+=current;
 			}else {
 				result*=current;
