3e57de4d52d9ea2236a2c660656a5d48b00a42e1
sem_in_github <semin6634@naver.com>
2025-03-06 13:58:51 +0900

    icoda q3 (불필요한 변수 제거)

--- src/icoda/greedy/q3.java
+++ src/icoda/greedy/q3.java
@@ -1,33 +1,27 @@
 package icoda.greedy;
-import java.util.*;
+import java.io.*;
 
 //O(N): 문자열 길이 < 10만
 public class q3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
 		
 		//변수 4개 or HashMap or int[]로 표현 가능할 듯
-		int[][] cnt = new int[2][2];//0,1의 cnt, temp 
+		int[] cnt = new int[2];//0,1의 cnt 
 		int turn = Integer.parseInt(str.charAt(0)+"");
-		cnt[turn][1]++;//temp
 		
 		for(int i=1; i<str.length(); i++) {
 			int cur = Integer.parseInt(str.charAt(i)+"");
-			if(cur == turn) {//temp++하기
-				cnt[turn][1]++;
-			}else {//turn 바꾸기 + cnt 늘리고 + temp 초기화
-				cnt[turn][0]++;
-				cnt[turn][1] = 0;
+			if(cur != turn) {//turn 바꾸기 + cnt 늘리기
+				cnt[turn]++;
 				turn = cur;
 			}
 		}
-		cnt[turn][0]++;
+		cnt[turn]++;
 		
-		System.out.println(Math.min(cnt[0][0], cnt[1][0]));
-		sc.close();
+		System.out.println(Math.min(cnt[0], cnt[1]));
 	}
-
 }
