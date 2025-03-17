6f4ea6cf753af27ef628efc6f519a26a6feabcfe
sem_in_github <semin6634@naver.com>
2025-03-17 15:50:00 +0900

    dp: n11726

---
+++ src/baekjoon/dp/n11726.java
@@ -0,0 +1,23 @@
+package baekjoon.dp;
+import java.io.*;
+
+public class n11726 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		if(n == 1 || n == 2) {
+			System.out.println(n);
+			return;
+		}
+		int[] dp = new int[n+1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i=3; i<n+1; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%10007;
+		}
+		System.out.println(dp[n]);
+	}
+
+}
